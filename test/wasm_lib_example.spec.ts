581a3c59dfe3a52a0c4f48c4717a3bf9da7d671a
tmtmt2209 <tmtmt2209@gmail.com>
2018-08-05 00:23:36 +0900

    Add: test spec file

---
+++ test/wasm_lib_example.spec.ts
@@ -0,0 +1,15 @@
+it('hash_with_secret', () =>
+    import('../lib/wasm_lib_example.js')
+        .then(wasm =>
+            wasm.hash_with_secret("some_message")
+        )
+        .then(array =>
+            expect(array)
+                .toEqual(new Uint8Array([
+                    157, 92, 228, 175, 135, 190, 204, 185,
+                    189, 175, 179, 183, 247, 34, 162, 147,
+                    44, 196, 233, 30, 168, 152, 53, 149,
+                    36, 74, 121, 240, 126, 107, 255, 201,
+                ]))
+        )
+)
