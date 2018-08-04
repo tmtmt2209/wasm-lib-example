27b94b9f35e2e856c49cfc74822d873296861449
tmtmt2209 <tmtmt2209@gmail.com>
2018-08-04 22:57:51 +0900

    Update: implement wasm function

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,35 @@
+#![feature(use_extern_macros)]
+
+extern crate octavo_digest as digest;
+extern crate octavo_mac as mac;
+extern crate wasm_bindgen;
+
+use mac::hmac::Hmac;
+use mac::Mac;
+use wasm_bindgen::prelude::*;
+
+#[wasm_bindgen]
+pub fn hash_with_secret(message: &str) -> Vec<u8> {
+    let secret = b"some_secret";
+
+    let mut hmac_sha256 = Hmac::<digest::sha2::Sha256>::new(secret);
+    hmac_sha256.update(message);
+
+    let mut output = [0; 32];
+    hmac_sha256.result(&mut output);
+
+    output.to_vec()
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
     #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
+    fn test_hash_with_secret() {
+        let except = vec![
+            157, 92, 228, 175, 135, 190, 204, 185, 189, 175, 179, 183, 247, 34, 162, 147, 44, 196,
+            233, 30, 168, 152, 53, 149, 36, 74, 121, 240, 126, 107, 255, 201,
+        ];
+        assert_eq!(except, hash_with_secret("some_message"));
     }
 }
